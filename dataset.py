72cf2ab2d8b5673c9e2422bd658893d843ad74f9
Dinh Duong <jenny.180820@gmail.com>
2025-08-08 16:53:34 +0700

    feat(data): build class read dataset

--- dataset.py
+++ dataset.py
@@ -35,14 +35,49 @@ class CIFARDataset(Dataset):
 class AnimalDataset(Dataset):
     def __init__(self, root, train=True, transform=None):
         self.image_paths = []
-        self.lables = []
+        self.labels = []
         self.categories = ["butterfly", "cat", "chicken", "cow", "dog", "elephant", "horse", "sheep", "spider",
                            "squirrel"]
         self.transform = transform
 
         data_path = os.path.join(root, "animals")
 
+        if train:
+            data_path = os.path.join(data_path, "train")
+        else:
+            data_path = os.path.join(data_path, "test")
+
+        for i, category in enumerate(self.categories):
+            data_files = os.path.join(data_path, category)
+            for item in os.listdir(data_files):
+                path = os.path.join(data_files, item)
+                self.image_paths.append(path)
+                self.labels.append(i)
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        image_path = self.image_paths[index]
+        image = Image.open(image_path).convert('RGB')
+        # image = cv2.imread(image_path)
+        # image = torch.from_numpy(image)
+        label = self.labels[index]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+
 if __name__ == '__main__':
+    # transform = Compose([
+    #     Resize((224, 224)),
+    #     ToTensor(),
+    # ])
+    # dataset = AnimalDataset(root="./data", train=True, transform=transform)
+    # image, label = dataset.__getitem__(5555)
+    # print(image.shape)
+    # print(label)
+
     dataset = CIFARDataset(root="./data", train=True)
     train_dataloader = DataLoader(
         dataset=dataset,
